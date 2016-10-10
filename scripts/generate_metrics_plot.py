8ee50afd15cf5e80f8f91b160bb10c1136dac9b9
Dirk von Grünigen <dirk@vongruenigen.com>
2016-10-10 22:14:59 +0200

    allow to specify which metrics to plot through a param

--- scripts/generate_metrics_plot.py
+++ scripts/generate_metrics_plot.py
@@ -6,18 +6,22 @@ import sys
 import json
 
 metrics_file = ''
-output_file = ''
+image_file = ''
+only_list = []
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'm:o:', ['metrics=', 'output='])
+    opts, args = getopt.getopt(sys.argv[1:], 'm:o:i',
+                               ['metrics=', 'image=', 'include='])
 except getopt.GetoptError:
-    print('./generate_metric_plot.py -m <metrics> -o <output.png>')
+    print('./generate_metric_plot.py -m <metrics> -i <image.png>')
     sys.exit(2)
 for opt, arg in opts:
     if opt in ('-m', '--metrics'):
         metrics_file = arg
-    elif opt in ('-o', '--output'):
-        output_file = arg
+    elif opt in ('-i', '--image'):
+        image_file = arg
+    elif opt in ('-o', '--only'):
+        only_list = arg.split(',')
 
 with open(metrics_file, 'r') as f:
     metrics = json.loads(f.read())
@@ -29,12 +33,13 @@ plt.xlabel('epoch')
 plt.ylabel('%')
 
 for name, values in metrics.items():
-    metric_names.append(name)
-    plt.plot(metrics[name])
+    if name in only_list or len(only_list) == 0:
+        metric_names.append(name)
+        plt.plot(metrics[name])
 
 plt.legend(metric_names, loc='upper left')
 
-if output_file == '':
+if image_file == '':
     plt.show()
 else:
-    plt.savefig(output_file)
+    plt.savefig(image_file + '.png')
