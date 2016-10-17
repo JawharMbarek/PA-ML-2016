4182a30c06e00a57094e0efdff9ca054b8c1cbe6
Dirk von Grünigen <dirk@vongruenigen.com>
2016-10-17 21:03:44 +0200

    make the visualiz word2vec vector script work

--- scripts/visualize_word_embeddings.py
+++ scripts/visualize_word_embeddings.py
@@ -3,16 +3,19 @@ import sys
 import getopt
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
+import pickle
 
 from gensim.models import Word2Vec 
 from sklearn.manifold import TSNE
 
 embeddings_file = ''
+frequencies_file = ''
 nb_words = 1000
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], 'e:n:',
-                               ['embeddings=', 'nr_of_words='])
+    opts, args = getopt.getopt(sys.argv[1:], 'e:f:n:',
+                               ['embeddings=', 'nr_of_words=', 'frequencies='])
 except getopt.GetoptError:
     print('./visualize_word_embeddings.py -e <embeddings> -n <nr-of-words>')
     sys.exit(2)
@@ -20,18 +23,22 @@ for opt, arg in opts:
     if opt in ('-e', '--embeddings'):
         embeddings_file = arg
     elif opt in ('-n', '--nr-of-words'):
-        nb_words = arg
- 
+        nb_words = int(arg)
+    elif opt in ('-f', '--frequencies-dictionary'):
+        frequencies_file = arg
+
 print('Loading embeddings...')
  
 word_vectors = {}
 word_embeddings = Word2Vec.load(embeddings_file)
+word_frequencies = pickle.load(open(frequencies_file, 'rb'))
+
+# select n-most used words
+word_tuples = sorted(word_frequencies.items(), reverse=True, key=operator.itemgetter(1))
+
 counter = 0
 
-import pdb
-pdb.set_trace()
-
-for word in word_embeddings.vocab.keys():
+for (word, freq) in word_tuples[:nb_words]:
     word_vectors[word] = word_embeddings[word]
     counter += 1
 
