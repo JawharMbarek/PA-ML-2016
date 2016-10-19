42603b18ec77f7f20687166a588292a43477a35b
Dirk von Grünigen <dirk@vongruenigen.com>
2016-10-19 20:53:59 +0200

    use datetime() instead of time() for generating the test id

--- model/utils.py
+++ model/utils.py
@@ -1,11 +1,10 @@
-from time import gmtime, strftime
+from datetime import datetime
 
 def generate_test_id(params=None):
     '''This function creates the id for a new test.'''
-    now = gmtime()
     name = 'test'
 
     if params is not None and 'name' in params:
         name = params['name']
 
-    return '%s-%s' % (strftime('%Y-%m-%d-%H-%M-%S', now), name)
+    return '%s-%s' % (datetime.now().strftime('%Y-%m-%d-%H-%M-%S'), name)
