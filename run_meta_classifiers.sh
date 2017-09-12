07d8f86798ac03b0d8c9a79f3706a32c5d4f3ef9
Dirk von Grünigen <dirk@vongruenigen.com>
2017-09-12 15:20:40 +0200

    update list of used domains in run_meta_classifiers.sh

--- run_meta_classifiers.sh
+++ run_meta_classifiers.sh
@@ -4,6 +4,7 @@ LIST=(DAI_tweets DIL_reviews HUL_reviews JCR_quotations \
       MPQ_reviews SEM_headlines SemEval_tweets TAC_reviews)
 
 LIST_DOMAINS=(
+  # SemEval13
   "dai,dil,hul,mpq,semeval,tac,jcr"
   "dai,dil,hul,mpq,semeval,tac,sem"
   "dai,dil,hul,mpq,semeval,jcr,sem"
@@ -13,6 +14,26 @@ LIST_DOMAINS=(
   "dai,hul,mpq,semeval,tac,jcr,sem"
   "dil,hul,mpq,semeval,tac,jcr,sem"
   "dai,dil,hul,mpq,semeval,tac,jcr,sem"
+  # SemEval16
+  "dai,dil,hul,mpq,semeval16,tac,jcr"
+  "dai,dil,hul,mpq,semeval16,tac,sem"
+  "dai,dil,hul,mpq,semeval16,jcr,sem"
+  "dai,dil,hul,mpq,tac,jcr,sem"
+  "dai,dil,hul,semeval16,tac,jcr,sem"
+  "dai,dil,mpq,semeval16,tac,jcr,sem"
+  "dai,hul,mpq,semeval16,tac,jcr,sem"
+  "dil,hul,mpq,semeval16,tac,jcr,sem"
+  "dai,dil,hul,mpq,semeval16,tac,jcr,sem"
+  # SemEval17
+  "dai,dil,hul,mpq,semeval17,tac,jcr"
+  "dai,dil,hul,mpq,semeval17,tac,sem"
+  "dai,dil,hul,mpq,semeval17,jcr,sem"
+  "dai,dil,hul,mpq,tac,jcr,sem"
+  "dai,dil,hul,semeval17,tac,jcr,sem"
+  "dai,dil,mpq,semeval17,tac,jcr,sem"
+  "dai,hul,mpq,semeval17,tac,jcr,sem"
+  "dil,hul,mpq,semeval17,tac,jcr,sem"
+  "dai,dil,hul,mpq,semeval17,tac,jcr,sem"
 )
 
 for domain in "${LIST[@]}"
